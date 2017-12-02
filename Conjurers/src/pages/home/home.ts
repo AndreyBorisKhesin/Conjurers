c5cec9c4ccae2e53c0f844809456487e0bd42d7e
christinekc <christinekc.cheng@gmail.com>
2017-12-02 13:48:12 -0500

    Merge

--- Conjurers/src/pages/home/home.ts
+++ Conjurers/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { Diagnostic } from 'ionic-native';
+import { CameraPreview, CameraPreviewRect } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,59 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+              public toastCtrl: ToastController) {
+      this.checkPermissions();
+  }
+   
+  checkPermissions() {
+    Diagnostic.isCameraAuthorized().then((authorized) => {
+        if(authorized)
+            this.initializePreview();
+        else {
+            Diagnostic.requestCameraAuthorization().then((status) => {
+                if(status == Diagnostic.permissionStatus.GRANTED)
+                    this.initializePreview();
+                else {
+                    // Permissions not granted
+                    // Therefore, create and present toast
+                    this.toastCtrl.create(
+                        {
+                            message: "Cannot access camera", 
+                            position: "bottom",
+                            duration: 5000
+                        }
+                    ).present();
+                }
+            });
+        }
+    });
+  }
 
+  initializePreview() {
+      // Make the width and height of the preview equal 
+      // to the width and height of the app's window
+      let previewRect: CameraPreviewRect = {
+        x: 0,
+        y: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+   
+    // Start preview
+    CameraPreview.startCamera(
+        previewRect, 
+        'rear', 
+        false, 
+        false, 
+        true,
+        1
+    );
+
+  }
+
+  takePicture() {
+      CameraPreview.takePicture({maxWidth: 320, maxHeight: 320});
   }
 
 }
