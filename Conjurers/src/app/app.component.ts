77d2663d5e1a97e7b1fb544c7ad662950c1a9ebc
christinekc <Chrsitine@Christines-MacBook-Pro.local>
2017-12-02 20:46:16 -0500

    please work

--- Conjurers/src/app/app.component.ts
+++ Conjurers/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
 
 import { HomePage } from '../pages/home/home';
 import { WelcomePage } from '../pages/welcome/welcome';
@@ -10,9 +9,6 @@ import { WelcomePage } from '../pages/welcome/welcome';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  private options: CameraPreviewOptions;
-  private cameraPreview: CameraPreview;
-
   rootPage:any = HomePage;
 
   constructor(platform: Platform) {
@@ -20,19 +16,6 @@ export class MyApp {
  
       StatusBar.styleDefault();
       Splashscreen.hide();
- 
-      this.options = {
-        x: 0,
-        y: 0,
-        width: window.screen.width,
-        height: window.screen.height,
-        camera: 'rear',
-        toBack: true,
-        tapPhoto: false,
-        previewDrag: false
-      };
- 
-      this.cameraPreview.startCamera(this.options);
  
     });
   }
--- Conjurers/src/pages/home/home.ts
+++ Conjurers/src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';i
  
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private options: CameraPreviewOptions;
+  private cameraPreview: CameraPreview;
  
   constructor(public navCtrl: NavController) {
- 
+    this.options = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      toBack: true,
+      tapPhoto: false,
+      previewDrag: false
+    };
+
+    this.cameraPreview.startCamera(this.options);
   }
  
   refresh(){
