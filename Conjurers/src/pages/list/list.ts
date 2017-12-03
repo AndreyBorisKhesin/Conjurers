1dcb6fe0c0d33116a1289d05533039d42163f8cd
christinekc <Chrsitine@Christines-MacBook-Pro.local>
2017-12-02 19:23:15 -0500

    trying ionic go

--- Conjurers/src/pages/list/list.ts
+++ Conjurers/src/pages/list/list.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
- 
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-list',
+  templateUrl: 'list.html'
 })
-export class HomePage {
- 
+export class ListPage {
+
   constructor(public navCtrl: NavController) {
- 
+
   }
- 
-  refresh(){
-    window['location'].reload();
-  }
- 
+
 }
