f7647148f663e8979be21890d692c4295a527d86
christinekc <christinekc.cheng@gmail.com>
2017-12-02 20:03:56 -0500

    patch

--- Conjurers/src/app/app.component.ts
+++ Conjurers/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { CameraPreview } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 import { WelcomePage } from '../pages/welcome/welcome';
