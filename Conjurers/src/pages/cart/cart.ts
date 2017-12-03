9f2bf903a6cc0948a4f505190677c01180431de8
christinekc <christinekc.cheng@gmail.com>
2017-12-03 07:39:22 -0500

    fix images

--- Conjurers/src/pages/cart/cart.ts
+++ Conjurers/src/pages/cart/cart.ts
@@ -20,79 +20,79 @@ export class CartPage {
                  public authService: AuthService) {
     this.items = [
       {
-        src: "../assets/imgs/images_for_products/apple.jpg",
+        src: "assets/imgs/images_for_products/apple.jpg",
         price: 1,
         title: "Apple",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/banana.jpg",
+        src: "assets/imgs/images_for_products/banana.jpg",
         price: 1,
         title: "Banana",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/black_coke.jpg",
+        src: "assets/imgs/images_for_products/black_coke.jpg",
         price: 1,
         title: "Coke zero",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/blue_oreos.jpg",
+        src: "assets/imgs/images_for_products/blue_oreos.jpg",
         price: 1,
         title: "Oreo Thins (original)",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/cadbury.jpg",
+        src: "assets/imgs/images_for_products/cadbury.jpg",
         price: 1,
         title: "Cadbury Chocolate Fingers",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/chex_mix.jpg",
+        src: "assets/imgs/images_for_products/chex_mix.jpg",
         price: 1,
         title: "Chex Mix (Original)",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/gray_coke.jpg",
+        src: "assets/imgs/images_for_products/gray_coke.jpg",
         price: 1,
         title: "Diet coke",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/hersheys.jpg",
+        src: "assets/imgs/images_for_products/hersheys.jpg",
         price: 1,
         title: "Hershey's drops",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/m&ms.jpg",
+        src: "assets/imgs/images_for_products/m&ms.jpg",
         price: 1,
         title: "m&ms",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/orange_chips.jpg",
+        src: "assets/imgs/images_for_products/orange_chips.jpg",
         price: 1,
         title: "Sun Chips (Harvest Cheddar)",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/pringles.jpg",
+        src: "assets/imgs/images_for_products/pringles.jpg",
         price: 1,
         title: "Pringles (Sour Cream and Onion)",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/red_coke.jpg",
+        src: "assets/imgs/images_for_products/red_coke.jpg",
         price: 1,
         title: "Coke (Original)",
         amount: 0
       },
       {
-        src: "../assets/imgs/images_for_products/yellow_oreos.jpg",
+        src: "assets/imgs/images_for_products/yellow_oreos.jpg",
         price: 1,
         title: "Golden Oreo Thins",
         amount: 0
@@ -102,7 +102,7 @@ export class CartPage {
   }
 
   pay() {
-    
+
   }
 
   total() {
@@ -113,7 +113,7 @@ export class CartPage {
     }
     return total;
   }
-  
+
   takePicture() {
     const options: CameraOptions = {
       quality: 50,
